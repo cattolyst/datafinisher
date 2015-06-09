dfa1865ac65c8c4c577980ad0ac8cea72428f697
root <root@localhost>
2015-06-09 07:50:33 -0500

    A code snippet and a note for how to better shorten long medical names

--- df.py
+++ df.py
@@ -19,3 +19,8 @@ print 'v'+str(rows[20][0])+'_'+'_'.join(foo[:3])
 """
 print 'v'+str(rows[20][0])+'_'+'_'.join(foo[:3]+foo[-2:])
 
+""" How to remove just the non-leading vowels in a string (\B means not-word-boundary)"""
+print re.sub("\B[aeiouyAEIOUY]+","","Outback Steakhouse")
+""" As to which words to keep, http://stackoverflow.com/questions/4105201/create-short-human-readable-string-from-longer-string
+Briefly, given a target string length, take words in decreasing length order until length exceeded; drop the one that overshoots;
+Return them in their original order of ocurrence """
