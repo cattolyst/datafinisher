02892ce61f03eda28c04aab2ddd75f48d5fe4ee5
root <root@localhost>
2015-06-09 10:25:41 -0500

    Added some more code examples and comments for shortening phrases and shortening words within phrases

--- df.py
+++ df.py
@@ -21,6 +21,14 @@ print 'v'+str(rows[20][0])+'_'+'_'.join(foo[:3]+foo[-2:])
 
 """ How to remove just the non-leading vowels in a string (\B means not-word-boundary)"""
 print re.sub("\B[aeiouyAEIOUY]+","","Outback Steakhouse")
+""" And remove duplicated letters """
+print re.sub(r"([a-z])\1",r"\1",re.sub("\B[aeiouyAEIOUY]+","","Aftercare following surgery system NEC"))
 """ As to which words to keep, http://stackoverflow.com/questions/4105201/create-short-human-readable-string-from-longer-string
 Briefly, given a target string length, take words in decreasing length order until length exceeded; drop the one that overshoots;
-Return them in their original order of ocurrence """
+Return them in their original order of ocurrence. When there are ties, take the right-most word. """
+""" Here is how to find the length of each word in a string """
+[len(ii) for ii in "Aftercare following surgery system NEC".split()]
+""" Then we find the max and min, iterate from max to min
+For every word of length ii, add ii to the score, an at the same time pop the largest index out of the array
+When score exceeds target length, break out of the loop before popping the corresponding index
+Order the indexes and extract the corresponding words"""
