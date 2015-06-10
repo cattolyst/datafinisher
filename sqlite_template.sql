0dd18079fec60f451a72af9c193a3c6998842e94
root <root@localhost>
2015-06-09 22:51:49 -0500

    Lots of changes-- running an external (hardcoded) sql script, creating the cdid (concept_code - id mapping) table, also the data_dictionary table with lots of information that will soon be useful for programatically making decisions about how to handle various types of columns that haven't been explicitly defined

--- sqlite_template.sql
+++ sqlite_template.sql
@@ -14,3 +14,22 @@ order by patient_num,start_date,encounter_num,observation_fact.concept_cd,modifi
 dcat2(pn, st, vid, datconcat) as
 (select patient_num pn,start_date st,vid,group_concat(datconcat,';') datconcat from dcat
 group by patient_num,start_date,vid),
+
+/* Query for dynamically generating SQL for pure code-only, no-mods tables
+
+select ' left join (select patient_num,start_date,group_concat(concept_cd,'';'') '||colid||' from cdid join observation_fact ob on ccd = concept_cd where id = '||cid||' group by patient_num,start_date order by patient_num,start_date) '||colid||' on '||colid||'.patient_num = scaffold.patient_num and '||colid||'.start_date = scaffold.start_date'
+from data_dictionary dd
+where coalesce(mod,dd.tval_char,dd.valueflag_cd,dd.units_cd,dd.confidence_num,dd.quantity_num,dd.location_cd,dd.valtype_cd,dd.nval_num,-1) = -1;
+
+-- then we mark those as done
+update data_dictionary 
+set done = 1
+where coalesce(mod,tval_char,valueflag_cd,units_cd,confidence_num,quantity_num,location_cd,valtype_cd,nval_num,-1) = -1;
+
+-- then we select the pure code-only with-mods tables...
+select * from data_dictionary 
+where done != 1 and mod is not null 
+and coalesce(tval_char,valueflag_cd,units_cd,confidence_num,quantity_num,location_cd,valtype_cd,nval_num,-1) = -1;
+
+*/
+
